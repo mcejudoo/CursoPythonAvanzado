2ef5f482db0ef81248f0cb1d3df09d000de475af
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2022-11-25 14:15:22 +0100

    rest
    rest

--- codigo/base_datos_rest_cliente.py
+++ codigo/base_datos_rest_cliente.py
@@ -1,6 +1,7 @@
 
 from requests import put, get, delete, post
 import json
+import pandas as pd
 
 URL = "http://localhost:5000/productos"
 
@@ -20,11 +21,16 @@ def testURLs():
     d = {"id":105,"nombre":"salsa brava","cat":{"id":2,"nombre":"comidas"},"precio":10.0,"exis":250}
     print(put(URL, data=json.dumps(d), headers=headers).json())
 
+
 def cargaCSVEmpleadosRest(path):
     """Cargar el fichero CSV de empleados a la base de datos utilizando el servicio REST"""
-    pass
+    dt = pd.read_csv(path, sep=';')
+    print(dt.head())
+
+    L = dt.to_json(orient='records', lines=True).splitlines()
+    print(L)
 
 
 if __name__ == '__main__':
     path = "../practicas/avanzado2/pandas/merge/Empleados.txt"
-    
+    cargaCSVEmpleadosRest(path)
