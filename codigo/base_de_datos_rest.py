0e212747500bfa8dcdc79bb6ded1d2d8eea3ab6e
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2022-11-25 10:37:52 +0100

    viernes 6
    viere

--- codigo/base_de_datos_rest.py
+++ codigo/base_de_datos_rest.py
@@ -23,11 +23,52 @@ class ProductoRest(Resource):
             abort(404, message=str(e))
 
 
+    def delete(self, id):
+        """
+        Eliminar un recurso de la base de datos
+        """
+        try:
+            bd = BaseDatos(path)
+            producto = bd.read(id)
+            d = {"Producto eliminado":producto.nombre}
+            bd.delete(id)
+            #return make_response(jsonify(d),200)        
+            return d
+        except Exception as e:
+            abort(404, message=str(e))
+
+
+class ProductosList(Resource):
+
+    def get(self):
+        """
+        Recuperar todos los productos de la base de datos
+        """
+        bd = BaseDatos(path)
+        productos = bd.select()
+        L = [p.to_json() for p in productos]
+        return make_response(jsonify(L),200)
+
+class ProductosCategoriasList(Resource):
+
+    def get(self, cat):
+        """
+        Recuperar todos los productos de la base de datos
+        """
+        bd = BaseDatos(path)
+        productos = bd.select(cat)
+        L = [p.to_json() for p in productos]
+        return make_response(jsonify(L),200)
+
+
 # Mapeo de clases y recursos:
 # GET: http://localhost:5000/productos/<id>
+# DELETE: http://localhost:5000/productos/<id>
 # GET: http://locaohost:5000/productos
 # GET: http://locaohost:5000/productos/categoria/<nombre_categoria>
 api.add_resource(ProductoRest, "/productos/<id>")
+api.add_resource(ProductosList, "/productos","/productos/")
+api.add_resource(ProductosCategoriasList, "/productos/categoria/<cat>")
 
 if __name__ == "__main__":
     app.run(debug=True)
