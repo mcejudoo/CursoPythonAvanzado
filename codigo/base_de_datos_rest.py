fe89d7ddafb42b5fcd19db0ea1af830b003e4a15
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2022-11-25 11:24:11 +0100

    viernes 8
    vie

--- codigo/base_de_datos_rest.py
+++ codigo/base_de_datos_rest.py
@@ -3,11 +3,23 @@ Servidor REST para implementar un API REST: con las operaciones CRUD
 """
 
 from flask import Flask, make_response, jsonify
-from flask_restful import Resource, Api, abort
+from flask_restful import Resource, Api, abort, reqparse
 from base_datos import BaseDatos, path, Producto, Categoria
 
 app = Flask(__name__)
 api = Api(app)
+
+# Definir parseadores para recuperar los parametros:
+parser_cat = reqparse.RequestParser()
+parser_cat.add_argument("id", type=int)
+parser_cat.add_argument("nombre", type=str)
+
+parser = reqparse.RequestParser()
+parser.add_argument("id", type=int)
+parser.add_argument("nombre", type=str)
+parser.add_argument("cat", type=dict)
+parser.add_argument("precio", type=float)
+parser.add_argument("exis", type=int)
 
 class ProductoRest(Resource):
 
@@ -21,7 +33,6 @@ class ProductoRest(Resource):
             return make_response(jsonify(producto.to_json()),200)        
         except Exception as e:
             abort(404, message=str(e))
-
 
     def delete(self, id):
         """
@@ -37,7 +48,6 @@ class ProductoRest(Resource):
         except Exception as e:
             abort(404, message=str(e))
 
-
 class ProductosList(Resource):
 
     def get(self):
@@ -49,6 +59,18 @@ class ProductosList(Resource):
         L = [p.to_json() for p in productos]
         return make_response(jsonify(L),200)
 
+    def post(self):
+        try:
+            args = parser.parse_args()
+            producto = Producto.create(args)
+            bd = BaseDatos(path)
+            bd.create(producto)
+            return {"create":1}
+
+        except Exception as e:
+            return {"error":str(e)}
+
+    
 class ProductosCategoriasList(Resource):
 
     def get(self, cat):
@@ -64,8 +86,9 @@ class ProductosCategoriasList(Resource):
 # Mapeo de clases y recursos:
 # GET: http://localhost:5000/productos/<id>
 # DELETE: http://localhost:5000/productos/<id>
-# GET: http://locaohost:5000/productos
-# GET: http://locaohost:5000/productos/categoria/<nombre_categoria>
+# GET: http://localhost:5000/productos
+# POST: http://localhost:5000/productos
+# GET: http://localhost:5000/productos/categoria/<nombre_categoria>
 api.add_resource(ProductoRest, "/productos/<id>")
 api.add_resource(ProductosList, "/productos","/productos/")
 api.add_resource(ProductosCategoriasList, "/productos/categoria/<cat>")
