46c099999b9fac26f0aa1a3e031a71b3c9e576ea
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2023-01-23 10:48:53 +0100

    lunes_10_48

--- codigo_ene_23/objetos.py
+++ codigo_ene_23/objetos.py
@@ -7,9 +7,12 @@ class Traductor:
     Implementación del traductor
     """
 
+    num_instancias = 0
+
     def __init__(self, idioma='es'):
         path = f"idiomas/{idioma}.txt"
         self.__palabras = self.__carga(path)
+        Traductor.num_instancias+=1
 
     def __carga(self, path):
         fich = None
@@ -28,11 +31,19 @@ class Traductor:
         finally:
             if fich: fich.close()
 
+    @staticmethod
+    def getNumInstancias():
+        return Traductor.num_instancias
+
     def getPalabra(self, k):
         return self.__palabras[k]
 
     def __str__(self):
         return "\n".join([k+" "+v] for k,v in self.palabras.items())
+
+    def __del__(self):
+        Traductor.num_instancias-=1
+        print(f'Quedan {Traductor.num_instancias} instancias ...')
 
 
 def test():
@@ -41,11 +52,13 @@ def test():
     """
     t = Traductor()
     print(t.getPalabra('facebook'))
-    
+    print('Num instancias: ', Traductor.getNumInstancias())
 
 
 if __name__ == '__main__':
     test()
 
     t = Traductor()
+    t2 = Traductor()
     print(t.getPalabra('facebook'))
+    print('Num instancias: ', Traductor.getNumInstancias())
