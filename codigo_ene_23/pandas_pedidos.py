77adf585adf7173e0f4b7c41da54259c264e1382
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2023-01-25 14:23:18 +0100

    miercoles_14_23

--- codigo_ene_23/pandas_pedidos.py
+++ codigo_ene_23/pandas_pedidos.py
@@ -1,6 +1,5 @@
 """
-Cargar el fichero de pedidos y crear un nuevo fichero por cada país, incluyendo
-sus pedidos. 
+Prácticas con pandas
 """
 
 import pandas as pd
@@ -8,6 +7,10 @@ import pandas as pd
 path='ficheros_templates/Pedidos.txt'
 
 def exportarPedidos():
+    """
+    Cargar el fichero de pedidos y crear un nuevo fichero por cada país, incluyendo
+    sus pedidos. 
+    """
     dt = pd.read_csv(path, sep=';')
     L = dt.pais.unique()
     for p in L:
@@ -18,5 +21,26 @@ def exportarPedidos():
         dtAux.to_csv(path_destino, sep=';', index=False, decimal=',')
         print('Generando fichero:', path_destino)
 
+
+def nuevasColsYFormatos():
+    """
+    Crear nuevas cols y exportar a formatos: xlsx, json, html
+    """
+    dt = pd.read_csv(path, sep=';')
+
+    # Añadir nuevas cols:
+    dt['porc_iva'] = 21.0
+    dt['iva'] = round(dt.importe * dt.porc_iva / 100, 2)
+
+    # Añadir una col: Total
+    dt['total'] = dt.importe + dt.iva
+    dt = dt[['idpedido','cliente','pais','importe','porc_iva','iva','total']]
+    dt.to_excel('pedidos.xlsx', index=False)
+
+    print(dt.head())
+
+
+
 if __name__ == '__main__':
-    exportarPedidos()
+    #exportarPedidos()
+    nuevasColsYFormatos()
