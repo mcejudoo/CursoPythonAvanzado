b5242148022b02fcd37e6c3bbe43ce028cb09168
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2023-01-26 09:17:22 +0100

    jueves_9_17

--- codigo_ene_23/pandas_names.py
+++ codigo_ene_23/pandas_names.py
@@ -3,16 +3,37 @@ Prácticas con los ficheros de los nacimientos de EEUU
 """
 
 import pandas as pd
+import os
 
 carpeta = "../practicas/avanzado2/pandas/names/"
 
 def cargarDT(path):
     dt = pd.read_csv(path, header=None, names=['nombre','sexo','cuenta'])
+    dt.set_index(["nombre","sexo"], inplace=True)
     return dt
 
-if __name__ == '__main__':
+def testSuma2():
     dt1 = cargarDT(carpeta+"yob1990.txt")
     dt2 = cargarDT(carpeta+"yob1991.txt")
-    r = dt1 + dt2
+    r = dt1.add(dt2, fill_value=0)
+    r.sort_values("cuenta", ascending=False, inplace=True)
     print(r.head())
+
+def filtroFicheros(ini, fin):
+    L = []
+    for f in os.listdir(carpeta):
+        t = f.partition('.')
+        if t[2] == 'txt':
+            año = int(t[0][-4:])
+            if ini <= año <= fin:
+                L.append(f)
+    return L
+
+
+def sumaRangoAños(ini, fin):
+    años = filtroFicheros(ini, fin)
+    print(años)
+
+if __name__ == '__main__':
+    sumaRangoAños(1990, 2000)
 
