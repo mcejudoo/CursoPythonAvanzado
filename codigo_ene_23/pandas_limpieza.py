2d00d159fc7dd7a38233ce024a6fc5abc28f0b03
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2023-01-26 11:04:27 +0100

    jueves_11_04

--- codigo_ene_23/pandas_limpieza.py
+++ codigo_ene_23/pandas_limpieza.py
@@ -9,11 +9,16 @@ def limpiarDT(path):
 
     # Limpiar espacios en blanco del nombre de las columnas.
     dt.columns = [col.strip() for col in dt.columns]
-    dt['Lat'] = pd.to_numeric(dt.Lat.str[:-1])
+    dt['Lat'] = pd.to_numeric(dt.Lat.str[:-1], downcast="float")
+    dt['Lon'] = pd.to_numeric(dt.Lon.str[:-1], downcast="float")
+    dt['Wind'] = pd.to_numeric(dt.Wind.str.replace(" mph",""), downcast='integer')
+    dt['Pressure'] = pd.to_numeric(dt.Pressure.str.replace(" mb",""), downcast='integer')
+    dt['DateTime'] = pd.to_datetime("2005 " + dt.Date + " " + dt.Time.str.replace(' GMT',''), \
+        infer_datetime_format=True)
+    dt.drop(columns=['Date','Time'], inplace=True)
     print(dt.head())
     print()
-    print(dt.info())
-
+    dt.info()
 
 if __name__ == '__main__':
     limpiarDT(carpeta+"IRMA.csv")
