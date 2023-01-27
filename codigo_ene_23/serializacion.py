cb157dfc155895d8c4d71c3ed63c0b1baaf3b49a
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2023-01-27 11:45:31 +0100

    viernes_11_45

--- codigo_ene_23/serializacion.py
+++ codigo_ene_23/serializacion.py
@@ -3,6 +3,7 @@ Serialización de objetos con pickle y Shelve
 """
 
 import pickle as p
+import shelve
 from base_datos import BaseDatos, Producto, Categoria, path
 
 def testPickleDump():
@@ -18,7 +19,6 @@ def testPickleDump():
     finally:
         if fich: fich.close()
 
-
 def testPickleLoad():
     fich = None
     try:       
@@ -32,7 +32,36 @@ def testPickleLoad():
     finally:
         if fich: fich.close()
 
+def testShelveWrite():        
+    try:
+        bd = BaseDatos(path)
+        L = bd.select('comidas')    
+        L2 = bd.select('bebidas')    
+        shelf = shelve.open('productos2')
+        shelf['comidas'] = L
+        shelf['bebidas'] = L2
+        shelf.close()
+    except Exception as e:
+        print(e)
+   
+def  testShelveRead():
+    try:
+        shelf = shelve.open('productos2')
+        L2 = shelf['bebidas']
+        L = shelf['comidas']     
+        shelf.close()
+        print('comidas: ')
+        print(L[:2])
+        print('bebidas: ', L2[:2])
+
+    except Exception as e:
+        raise e
+        print(e)
+
+
 
 if __name__ == '__main__':
-    testPickleDump()
-    testPickleLoad()
+    #testPickleDump()
+    #testPickleLoad()
+    testShelveWrite()
+    testShelveRead()
