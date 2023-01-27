a38bd29f223fe0f62a0e9a1822dc700e513f4b78
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2023-01-27 13:50:08 +0100

    viernes_13_49

--- codigo_ene_23/ws_rest_productos.py
+++ codigo_ene_23/ws_rest_productos.py
@@ -4,11 +4,23 @@ Servicio Rest con Flask para la gestión de los productos
 
 from base_datos import BaseDatos, Producto, Categoria, path
 from flask import Flask, make_response, jsonify, abort
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
 
 # Crear la aplicación y el API: __name__ nombre del módulo
 app = Flask(__name__)
 api = Api(app)
+
+# Definir parseadores para recuperar los parametros:
+parser_cat = reqparse.RequestParser()
+parser_cat.add_argument("id", type=int)
+parser_cat.add_argument("nombre", type=str)
+
+parser = reqparse.RequestParser()
+parser.add_argument("id", type=int)
+parser.add_argument("nombre", type=str)
+parser.add_argument("cat", type=dict)
+parser.add_argument("precio", type=float)
+parser.add_argument("exis", type=int)
 
 # Operaciones: GET -> id, DELETE -> id
 
@@ -24,6 +36,23 @@ class Productos(Resource):
         # Convertirlos a json:
         L = [p.to_json() for p in productos]
         return make_response(jsonify(L), 200)
+
+    def post(self):
+        """Crea un nuevo recurso: producto"""
+        try:
+            args = parser.parse_args()
+            #print('args:', args)
+            nombreCat = args['cat']['nombre']
+            bd = BaseDatos(path)
+            categoria = bd.readCategoria(nombreCat)
+            producto = Producto.create(args)
+            producto.cat = categoria
+            #print('producto: ', producto)           
+            n = bd.create(producto)
+            return {"create":n}
+
+        except Exception as e:
+            return {"error":str(e)}
 
 
 class ProductosCategorias(Resource):
